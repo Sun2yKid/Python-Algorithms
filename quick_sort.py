5390df5ef98140cd6c352baaf0c2eb530287eb62
zhonghui.yao <zhonghua00700@gmail.com>
2019-12-27 22:54:29 +0800

    add quick sort

---
+++ quick_sort.py
@@ -0,0 +1,20 @@
+"""
+Quick Sort
+average time  complexity: O(nlogn)
+average space complexity: O(logn)
+"""
+
+
+def quick_sort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
+print('*'*10, 'start', '*'*10)
+l = [234, 123412, 1324123, 12341, 351234, 235234, 26523452, 623462, 1]
+print(quick_sort(l))
